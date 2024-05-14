d6bc1696c6956738aad2e683afe7cdfd7da1b91a
mdeandwibekti <mdeandwibekti@gmail.com>
2024-05-14 17:27:22 +0700

    class mahasiswa

--- constuctor/constuctor.cpp
+++ constuctor/constuctor.cpp
@@ -4,4 +4,17 @@ using namespace std;
 class mahasiswa
 {
 
+public:
+	static int nim;
+	int id;
+	string nama;
+
+	void setid();
+
+	void printall();
+	mahasiswa(string pnama)
+	{
+		nama = pnama;
+		setid();
+	}
 };
