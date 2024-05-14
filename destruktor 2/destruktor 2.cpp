e6271eead6970b7b5d79337221ea23dcd9926b30
mdeandwibekti <mdeandwibekti@gmail.com>
2024-05-14 18:57:04 +0700

    angka detructor

--- destruktor 2/destruktor 2.cpp
+++ destruktor 2/destruktor 2.cpp
@@ -23,6 +23,25 @@ angka::angka(int i)  // Constructor
     isiData();
 }
 
+angka::~angka()       // Destructor
+{
+    cout << endl;
+    cetakData();
+    delete[]arr;
+    cout << "Alamat Array Sudah Dilepaskan" << endl;
+}
+
+void angka::cetakData()
+{
+    for (int i = 1; i <= panjang; i++)
+    {
+        cout << i << " = " << arr[i] << endl;
+    }
+}
+
+
+
+
 
 
 
