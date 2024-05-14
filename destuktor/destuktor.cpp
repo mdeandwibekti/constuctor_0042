0d0f3f8ebcd12219ac5e84179ca64b15f381c77e
mdeandwibekti <mdeandwibekti@gmail.com>
2024-05-14 18:47:54 +0700

    void cetak

--- destuktor/destuktor.cpp
+++ destuktor/destuktor.cpp
@@ -28,6 +28,28 @@ public:
 		nim = iNim;
 		nama = iNama;
 	};
+	void cetak()
+	{
+		cout << "NIM    = " << nim << endl;
+		cout << "Nama   = " << nama << endl;
+		cout << endl;
+	}
+};
+
+int main()
+{
+	mahasiswa mhs1;
+	mahasiswa mhs2(20);
+	mahasiswa mhs3("Indra");
+	mahasiswa mhs4(30, "Fauzan");
+
+	mhs1.cetak();
+	mhs2.cetak();
+	mhs3.cetak();
+	mhs4.cetak();
+
+	return 0;
+}
 
 
 };
