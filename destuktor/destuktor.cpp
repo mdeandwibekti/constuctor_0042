2bb30f4065056a5c9d243883f4ab8b7d71f508fa
mdeandwibekti <mdeandwibekti@gmail.com>
2024-05-14 18:54:02 +0700

    class angka

--- destuktor/destuktor.cpp
+++ destuktor/destuktor.cpp
@@ -5,48 +5,51 @@ using namespace std;
 class mahasiswa
 {
 public:
-	int nim;
-	string nama;
+    int nim;
+    string nama;
 
 public:
-	mahasiswa()
-	{
-		nim = 0;
-		nama = "";
-	};
-	mahasiswa(int iNim)
-	{
-		nim = iNim;
-	};
+    mahasiswa()
+    {
+        nim = 0;
+        nama = "";
+    };
 
-	mahasiswa(string iNama)
-	{
-		nama = iNama;
-	};
-	mahasiswa(int iNim, string iNama)
-	{
-		nim = iNim;
-		nama = iNama;
-	};
-	void cetak()
-	{
-		cout << "NIM    = " << nim << endl;
-		cout << "Nama   = " << nama << endl;
-		cout << endl;
-	}
+    mahasiswa(int iNim)
+    {
+        nim = iNim;
+    };
+
+    mahasiswa(string iNama)
+    {
+        nama = iNama;
+    };
+
+    mahasiswa(int iNim, string iNama)
+    {
+        nim = iNim;
+        nama = iNama;
+    };
+
+    void cetak()
+    {
+        cout << "NIM    = " << nim << endl;
+        cout << "Nama   = " << nama << endl;
+        cout << endl;
+    }
 };
 
 int main()
 {
-	mahasiswa mhs1;
-	mahasiswa mhs2(20);
-	mahasiswa mhs3("Indra");
-	mahasiswa mhs4(30, "Fauzan");
+    mahasiswa mhs1;
+    mahasiswa mhs2(20);
+    mahasiswa mhs3("Indra");
+    mahasiswa mhs4(30, "Fauzan");
 
-	mhs1.cetak();
-	mhs2.cetak();
-	mhs3.cetak();
-	mhs4.cetak();
+    mhs1.cetak();
+    mhs2.cetak();
+    mhs3.cetak();
+    mhs4.cetak();
 
-	return 0;
-};
+    return 0;
+}
